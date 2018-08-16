ffbe61f8faf861f772bf6a19944edd109068e8e6
pragnesh pogaru <pragneshpogaru@gmail.com>
2018-08-17 03:28:11 +0530

    Even numbers and age using if condition

---
+++ New folder/Even/src/Age.java
@@ -0,0 +1,15 @@
+
+public class Age {
+
+	public static void main(String[] args) {
+		int Age=65;
+				if(Age>=15&&Age<=20)
+			System.out.println("Tennage");
+		else if(Age>20&&Age<=50)
+		System.out.println("Adult");
+		else
+			System.out.println("Old");
+
+	}
+
+}
---
+++ New folder/Even/src/Even.java
@@ -0,0 +1,13 @@
+
+public class Even {
+
+	public static void main(String[] args) {
+		int num=3;
+		System.out.println("enter the value of num");
+		if(num%2==0)
+			System.out.println("even");
+		else
+			System.out.println("odd");
+	}
+
+}
